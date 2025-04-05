7e10ee4723dfb45966e70ca551da7587602e3b0f
hydrapal <rahulpal.innovative@gmail.com>
2025-04-05 20:55:01 +0530

    Deploy updated Angular build

--- core.js
+++ core.js
@@ -3,8 +3,8 @@ class cdsSDK {
     constructor(options = {}) {
       this.options = {
         componentName: 'cardexscan-swap-root',
-        scriptUrl: `https://cdn.jsdelivr.net/gh/hydracds/swap-sdk@v${options.version || '0.0.6'}/widget.js`,
-        styleUrl: `https://cdn.jsdelivr.net/gh/hydracds/swap-sdk@v${options.version || '0.0.6'}/styles.css`,
+        scriptUrl: `https://cdn.jsdelivr.net/gh/hydracds/swap-sdk@v${options.version || '0.0.7'}/widget.js`,
+        styleUrl: `https://cdn.jsdelivr.net/gh/hydracds/swap-sdk@v${options.version || '0.0.7'}/styles.css`,
         version: 'latest',
         ...options
       };
